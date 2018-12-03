00927bbbd6519253114cbcca2609718e0eb08ae4
Carlos Pinho <carlos.p37@gmail.com>
2018-12-03 03:50:18 +0000

    Já desenha ao clicar

--- ReversISEC/app/src/main/java/pt/isec/a21240456/a2120528/reversisec/SinglePlayer.java
+++ ReversISEC/app/src/main/java/pt/isec/a21240456/a2120528/reversisec/SinglePlayer.java
@@ -6,17 +6,21 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import pt.isec.a21240456.a2120528.reversisec.Board;
 
 public class SinglePlayer extends AppCompatActivity {
 
     private final int maxN = 8;
     private ImageView[][] ivCells = new ImageView[maxN][maxN];
-    private Context context;
     private Drawable[] drawCells = new Drawable[3];
+    private Context context;
     private Board board = new Board();
+    private int playerTurn = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +30,8 @@ public class SinglePlayer extends AppCompatActivity {
         context = this;
 
         loadResources();
-
-        designGameBoard();
+        configGameBoard();
+        drawBoard();
     }
 
     private void loadResources() {
@@ -37,8 +41,7 @@ public class SinglePlayer extends AppCompatActivity {
 
     }
 
-    private void designGameBoard(){
-
+    private void configGameBoard(){
         LinearLayout llGameBoard = findViewById(R.id.gameBoard);
         int cellSize = Math.round(getScreenSizeMinusPadding() / maxN);
 
@@ -49,7 +52,37 @@ public class SinglePlayer extends AppCompatActivity {
             LinearLayout linRow = new LinearLayout(context);
             for (int j = 0; j < maxN; j++) {
                 ivCells[i][j] = new ImageView(context);
+                final int x = i;
+                final int y = j;
+                ivCells[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(playerTurn == 1){
+                            //Toast.makeText(context, "(X: " + x + "; Y: " + y + ")", Toast.LENGTH_SHORT).show();
+                            board.addPiece('w', x, y);
+                            drawBoard();
+                        }else{
+                            Toast.makeText(context, "Not your turn", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                linRow.addView(ivCells[i][j], llCell);
+            }
+            llGameBoard.addView(linRow, llRow);
+        }
+    }
 
+    private float getScreenSizeMinusPadding(){
+        return context.getResources().getDisplayMetrics().widthPixels - dpToPixels(32, context);
+    }
+
+    public static int dpToPixels(float dp, Context context) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+    }
+
+    private void drawBoard() {
+        for (int i = 0; i < maxN; i++) {
+            for (int j = 0; j < maxN; j++) {
                 switch(board.getCell(i, j)) {
                     case ' ':
                         ivCells[i][j].setBackground(drawCells[0]);
@@ -61,30 +94,7 @@ public class SinglePlayer extends AppCompatActivity {
                         ivCells[i][j].setBackground(drawCells[2]);
                         break;
                 }
-
-                linRow.addView(ivCells[i][j], llCell);
             }
-            llGameBoard.addView(linRow, llRow);
         }
-
-        /*for (int i = 0; i < maxN; i++) {
-            LinearLayout linRow = new LinearLayout(context);
-            for (int j = 0; j < maxN; j++) {
-                ivCells[i][j] = new ImageView(context);
-                ivCells[i][j].setBackground(drawCells[0]);
-                linRow.addView(ivCells[i][j], llCell);
-            }
-            llGameBoard.addView(linRow, llRow);
-        }*/
-
-
-    }
-
-    private float getScreenSizeMinusPadding(){
-        return context.getResources().getDisplayMetrics().widthPixels - dpToPixels(30, context);
-    }
-
-    public static int dpToPixels(float dp, Context context) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
     }
 }
